562de007da929e88fd6754c6cfbdf3c9d73af8b9
Eric Korolev <3254@mail.ru>
2017-08-30 09:11:08 +0500

    Get leads list

--- auth.php
+++ auth.php
@@ -5,7 +5,7 @@ $user=array(
 	'USER_HASH'=>'16d7953691364ef1ff4207702c429960' #Хэш для доступа к API (смотрите в профиле пользователя)
 );
 
-$subdomain='test'; #Наш аккаунт - поддомен
+$subdomain='new59a53e074f795'; #Наш аккаунт - поддомен
 #Формируем ссылку для запроса
 $link='https://'.$subdomain.'.amocrm.ru/private/api/auth.php?type=json';
 $curl=curl_init(); #Сохраняем дескриптор сеанса cURL
@@ -24,7 +24,7 @@ curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
 $out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
 $code=curl_getinfo($curl,CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
 curl_close($curl); #Завершаем сеанс cURL
-CheckCurlResponse($code);
+CheckCurlResponse($code); #Проверка кода ответа сервера
 /**
  * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
  * нам придётся перевести ответ в формат, понятный PHP
--- leads_list.php
+++ leads_list.php
@@ -1,6 +1,5 @@
 <?php
 
-$subdomain='new59a53e074f795'; #Наш аккаунт - поддомен
 #Формируем ссылку для запроса
 $link='https://'.$subdomain.'.amocrm.ru/private/api/v2/json/leads/list';
 
@@ -16,30 +15,10 @@ curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
 curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,0);
 
 $out=curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
-$code=curl_getinfo($curl,CURLINFO_HTTP_CODE);
-curl_close($curl);
+$code=curl_getinfo($curl,CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
+curl_close($curl); #Завершаем сеанс cURL
 
-$code=(int)$code;
-$errors=array(
-  301=>'Moved permanently',
-  400=>'Bad request',
-  401=>'Unauthorized',
-  403=>'Forbidden',
-  404=>'Not found',
-  500=>'Internal server error',
-  502=>'Bad gateway',
-  503=>'Service unavailable'
-);
-try
-{
-  #Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
-  if($code!=200 && $code!=204)
-    throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error',$code);
-}
-catch(Exception $E)
-{
-  die('Ошибка: '.$E->getMessage().PHP_EOL.'Код ошибки: '.$E->getCode());
-}
+CheckCurlResponse($code); #Проверка кода ответа сервера
  
 /**
  * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
@@ -47,4 +26,7 @@ catch(Exception $E)
  */
 $Response=json_decode($out,true);
 $Response=$Response['response'];
+
+// Раздел отладки
+echo '<pre>'; print_r($Response); echo '</pre>';
 ?>
