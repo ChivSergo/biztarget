18ba160b43893240fe6ea5571f31c7438367e917
Eric Korolev <3254@mail.ru>
2017-08-31 12:54:20 +0500

    Add error handling

--- leads_id.php
+++ leads_id.php
@@ -9,21 +9,22 @@ $leads_id = array();
 */
 	
 // Записываем в массив ID сделок, у которых нет задач.
-
+/*
 foreach($leads_list['leads'] as $leads) {
 		if ($leads['closest_task'] == 0) {
 			array_push($leads_id, $leads['id']);
 		}
 	}
+*/
 
-	
-// Разрабатываю то же действие, но с отловом возможных ошибок
-/*
-if(isset($leads_list['leads']) {
+// Проверяем, есть ли доступ к данным и записываем в массив ID сделок, у которых нет задач.
+if(isset($leads_list['leads'])) {
 	
 	foreach($leads_list['leads'] as $leads) {
-		if(is_array($leads) && isset($leads['id']) {
-			array_push($leads_id, $leads['id']);
+		if(is_array($leads) && isset($leads['id'])) {
+			if ($leads['closest_task'] == 0) {
+				array_push($leads_id, $leads['id']);
+			}
 		} else {
 			die('Невозможно получить поле "ID сделки"');
 		} 
@@ -32,9 +33,7 @@ if(isset($leads_list['leads']) {
 } else {
 	die('Невозможно получить "Список сделок"');
 }
-*/
-
 	
 // Раздел отладки
-// echo '<pre>'; print_r($leads_id); echo '</pre>';
+echo '<pre>'; print_r($leads_id); echo '</pre>';
 ?>
